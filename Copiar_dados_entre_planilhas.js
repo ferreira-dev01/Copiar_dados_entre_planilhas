990bb2651dfa67e58faae3f7d8098370e8179632
FerreiraDev <nicoguitarreiro@gmail.com>
2024-12-25 23:32:03 -0300

    inicio repositorio

---
+++ Copiar_dados_entre_planilhas.js
@@ -0,0 +1,16 @@
+    /*
+    * Copia os dados de uma planilha para outra mesmo sendo de drives diferentes
+    */
+
+    // ler valores
+    let sheet_ler = SpreadsheetApp.openByUrl('https://docs.google.com/spreadsheets/d/1JJtDgtd9ZPCV2D5xdFIxecV0YWtc9kqk20Z4l8PKIUs/edit');
+    sheet_ler = sheet_ler.getSheetByName("plan1");
+    valores_lidos = sheet_ler.getRange('A1:D4').getValues();
+
+   // seleciona planilha para gravar ranger
+    let sheet_gravar = SpreadsheetApp.openByUrl('https://docs.google.com/spreadsheets/d/1ZDirn6M3rDZ8oYr5uHnylDcr86yuh5FCy5pwseOj2a4/edit');
+    sheet_gravar = sheet_gravar.getSheetByName("plan1");
+
+
+     // grava em ranger do mesmo tamanho do ranger copiado
+     sheet_gravar.getRange('A1:D4').setValues(valores_lidos);
